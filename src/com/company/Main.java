495f55fd3ef22902fa43f1bae6cdabf04577c1ff
Tomasz Dudziński <tomasz.dudzinski.sda@tlen.pl>
2019-07-07 12:10:23 +0200

    Tomek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1 master");
         System.out.println("Marek");
+        System.out.println("Tomek");
     }
 }
