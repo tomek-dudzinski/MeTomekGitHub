df8e6ebad129cf262d0fdf8bc14e38e2d35f05b4
Marek Michalski <nydmif@gmail.com>
2019-07-07 12:44:35 +0200

    Nowa klasa Marek

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1 master - Marek");
+
         System.out.println("Marek");
         System.out.println("Tomek");
 
